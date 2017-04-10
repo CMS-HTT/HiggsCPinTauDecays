0d52324e495fe5d2db7873b35fd2c518b46917a2
Claudia Pistone <pistone@physik.rwth-aachen.de>
2017-04-10 17:27:59 +0200

    removed comments

--- python/AdvancedRefitVertexProducer_cfi.py
+++ python/AdvancedRefitVertexProducer_cfi.py
@@ -5,25 +5,17 @@ AdvancedRefitVertexBSProducer = cms.EDProducer(
 	combineNLeptons = cms.int32(2),
 	srcCands = cms.InputTag("packedPFCandidates"),
 	srcLostTracks = cms.InputTag("lostTracks"),
-	#TauTag = cms.InputTag("slimmedTaus"),
 	PVTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
 	deltaRThreshold = cms.double(0.001),
 	deltaPtThreshold = cms.double(0.001),
 
-	#TrackCollectionTag = cms.InputTag("NonTauPairTrackCollectionProducer"),
-	#PVTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
 	beamSpot = cms.InputTag("offlineBeamSpot"),
 	useBeamSpot = cms.bool(True),
 	useLostCands = cms.bool(True),
 
 	srcElectrons = cms.InputTag("slimmedElectrons"),
-	#electronTypeID = cms.InputTag("Tight"),
 	srcMuons = cms.InputTag("slimmedMuons"),
-	#muonTypeID = cms.InputTag("Tight"),
 	srcTaus = cms.InputTag("slimmedTaus"),
-	#tauTypeID = cms.InputTag("Loose"),
-
-	#srcLeptons = cms.VInputTag(srcElectrons+electronTypeID, srcMuons+muonTypeID, srcTaus+tauTypeID+"Cleaned"),
 	srcLeptons = cms.VInputTag(cms.InputTag("slimmedElectrons"), cms.InputTag("slimmedMuons"), cms.InputTag("slimmedTaus")),
 )
 
@@ -32,24 +24,17 @@ AdvancedRefitVertexNoBSProducer = cms.EDProducer(
 	combineNLeptons = cms.int32(2),
 	srcCands = cms.InputTag("packedPFCandidates"),
 	srcLostTracks = cms.InputTag("lostTracks"),
-	#TauTag = cms.InputTag("slimmedTaus"),
 	PVTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
 	deltaRThreshold = cms.double(0.001),
 	deltaPtThreshold = cms.double(0.001),
 
-	#TrackCollectionTag = cms.InputTag("NonTauPairTrackCollectionProducer"),
-	#PVTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
 	beamSpot = cms.InputTag("offlineBeamSpot"),
 	useBeamSpot = cms.bool(False),
 	useLostCands = cms.bool(True),
 
 	srcElectrons = cms.InputTag("slimmedElectrons"),
-	#electronTypeID = cms.InputTag("Tight"),
 	srcMuons = cms.InputTag("slimmedMuons"),
-	#muonTypeID = cms.InputTag("Tight"),
 	srcTaus = cms.InputTag("slimmedTaus"),
-	#tauTypeID = cms.InputTag("Loose"),
-	#srcLeptons = cms.VInputTag(srcElectrons+electronTypeID, srcMuons+muonTypeID, srcTaus+tauTypeID+"Cleaned"),
 	srcLeptons = cms.VInputTag(cms.InputTag("slimmedElectrons"), cms.InputTag("slimmedMuons"), cms.InputTag("slimmedTaus")),
 )
 
