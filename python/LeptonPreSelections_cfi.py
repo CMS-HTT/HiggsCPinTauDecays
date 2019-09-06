0ff1511a8fbd71ac06bb3c451105b6f16cf98ea3
Aruna Nayak <Aruna.Nayak@cern.ch>
2019-09-06 11:48:35 +0200

    simplifying lepton filters

--- python/LeptonPreSelections_cfi.py
+++ python/LeptonPreSelections_cfi.py
@@ -2,21 +2,16 @@ import FWCore.ParameterSet.Config as cms
 
 filteredMuons = cms.EDFilter("PATMuonSelector",
                              src = cms.InputTag("slimmedMuons"),
-                             cut = cms.string("pt > 20 && " + "abs(eta) < 2.4" +
-                                              " && isGlobalMuon"+
-                                              " && (pfIsolationR04.sumChargedHadronPt+max(0.,(pfIsolationR04.sumNeutralHadronEt+pfIsolationR04.sumPhotonEt-0.5*pfIsolationR04.sumPUPt)))/pt < 0.3"
-                                              )
+                             cut = cms.string("pt > 20 && " + "abs(eta) < 2.4")
                              )
 filteredElectrons = cms.EDFilter("PATElectronSelector",
                                  src = cms.InputTag("slimmedElectrons"),
-                                 cut = cms.string("pt > 20 && " + "abs(eta) < 2.4"+
-                                                  " && (pfIsolationVariables.sumChargedHadronPt+max(0.,(pfIsolationVariables.sumNeutralHadronEt+pfIsolationVariables.sumPhotonEt-0.5*pfIsolationVariables.sumPUPt)))/pt < 0.3"
-                                                  )
+                                 cut = cms.string("pt > 20 && " + "abs(eta) < 2.4")
                                  )
 filteredTaus = cms.EDFilter("PATTauSelector",
                             src = cms.InputTag("NewTauIDsEmbedded"),
                             cut = cms.string("tauID('decayModeFindingNewDMs') > 0.5" +
-                                             " && (tauID('byVLooseIsolationMVArun2017v2DBnewDMwLT2017') > 0.5 || tauID('byVVLooseDeepTau2017v2p1VSjet') > 0.5)"
+                                             " && (tauID('byVVLooseIsolationMVArun2017v2DBoldDMwLT2017') > 0.5 || tauID('byVVVLooseDeepTau2017v2p1VSjet') > 0.5)"
                                              )
                             )
 
